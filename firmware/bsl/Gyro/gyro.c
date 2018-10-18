419fe4e66df8023793b43c1bf7234765661729f1
tahardi <Taylor.A.Hardin.GR@dartmouth.edu>
2018-10-18 11:36:49 -0400

    updated gyro library to support kited

--- firmware/bsl/Gyro/gyro.c
+++ firmware/bsl/Gyro/gyro.c
@@ -15,211 +15,206 @@
   BSD license, all text above must be included in any redistribution
   ------------------------
   Adapted for Amulet by Josiah Hester (7/26/17)
+  ------------------------
+  Adapted for LSM6DSL by Taylor Hardin (6/25/18)
  ****************************************************/
-
 #include "gyro.h"
 #include "ADXL362driver/spi_430.h"
 #include "bsp_init.h" // To configure for bracelet or dev kit
 #include "ADXL362driver/util.h"
 
-void __L3GD20_write8(l3gd20Registers_t reg, uint8_t value)
-{
-  GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
-  spi_send(reg);
-  spi_send(value);
-  GYRO_CS_PxOUT |= GYRO_CS_BITx;
+
+void __L3GD20_write8(l3gd20Registers_t reg, uint8_t value) {
+    GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
+    spi_send(reg);
+    spi_send(value);
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
 }
 
-uint8_t __L3GD20_read8(l3gd20Registers_t reg)
-{
-  uint8_t value;
-  GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
-  spi_send((uint8_t)reg | 0x80); // set READ bit
-  value = spi_send(0xFF);
-  GYRO_CS_PxOUT |= GYRO_CS_BITx;
-  return value;
+void __LSM6DSL_write8(lsm6dslRegisters_t reg, uint8_t value) {
+    GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
+    spi_send(reg);
+    spi_send(value);
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
 }
 
 
-int8_t GyroBegin()
-{
-    //pinMode(_cs, OUTPUT);
-    //SPI.begin();
-    //SPI.setBitOrder(MSBFIRST); // I think this is default actually
-    //SPI.setDataMode(SPI_MODE0); //CPHA = CPOL = 0    MODE = 0
-
-    //pinMode(_clk, OUTPUT);
-    //pinMode(_mosi, OUTPUT);
-    //pinMode(_miso, INPUT);
-    //digitalWrite(_cs, HIGH);
-
-  // Turn on power to Gyro
-  GYRO_ENABLE_PxDIR |= GYRO_ENABLE_BITx;
-  GYRO_ENABLE_PxOUT &= ~GYRO_ENABLE_BITx;
-
-  // Chip select high for Gyro
-  GYRO_CS_PxDIR |= GYRO_CS_BITx;
-  GYRO_CS_PxOUT |= GYRO_CS_BITx;
-
-  address = L3GD20_ADDRESS;
-  range = L3DS20_RANGE_250DPS;
-
-  /* Make sure we have the correct chip ID since this checks
-     for correct address and that the IC is properly connected */
-  uint8_t id = __L3GD20_read8(L3GD20_REGISTER_WHO_AM_I);
-  //Serial.println(id, HEX);
-  if ((id != L3GD20_ID) && (id != L3GD20H_ID))
-  {
-    return 0;
-  }
-
-  /* Set CTRL_REG1 (0x20)
-   ====================================================================
-   BIT  Symbol    Description                                   Default
-   ---  ------    --------------------------------------------- -------
-   7-6  DR1/0     Output data rate                                   00
-   5-4  BW1/0     Bandwidth selection                                00
-     3  PD        0 = Power-down mode, 1 = normal/sleep mode          0
-     2  ZEN       Z-axis enable (0 = disabled, 1 = enabled)           1
-     1  YEN       Y-axis enable (0 = disabled, 1 = enabled)           1
-     0  XEN       X-axis enable (0 = disabled, 1 = enabled)           1 */
-
-  /* Switch to power down mode and enable all three channels */
-  GyroPowerDownMode();
-
-  /* ------------------------------------------------------------------ */
-
-  /* Set CTRL_REG2 (0x21)
-   ====================================================================
-   BIT  Symbol    Description                                   Default
-   ---  ------    --------------------------------------------- -------
-   5-4  HPM1/0    High-pass filter mode selection                    00
-   3-0  HPCF3..0  High-pass filter cutoff frequency selection      0000 */
-
-  /* Nothing to do ... keep default values */
-  /* ------------------------------------------------------------------ */
-
-  /* Set CTRL_REG3 (0x22)
-   ====================================================================
-   BIT  Symbol    Description                                   Default
-   ---  ------    --------------------------------------------- -------
-     7  I1_Int1   Interrupt enable on INT1 (0=disable,1=enable)       0
-     6  I1_Boot   Boot status on INT1 (0=disable,1=enable)            0
-     5  H-Lactive Interrupt active config on INT1 (0=high,1=low)      0
-     4  PP_OD     Push-Pull/Open-Drain (0=PP, 1=OD)                   0
-     3  I2_DRDY   Data ready on DRDY/INT2 (0=disable,1=enable)        0
-     2  I2_WTM    FIFO wtrmrk int on DRDY/INT2 (0=dsbl,1=enbl)        0
-     1  I2_ORun   FIFO overrun int on DRDY/INT2 (0=dsbl,1=enbl)       0
-     0  I2_Empty  FIFI empty int on DRDY/INT2 (0=dsbl,1=enbl)         0 */
-
-  /* Nothing to do ... keep default values */
-  /* ------------------------------------------------------------------ */
-
-  /* Set CTRL_REG4 (0x23)
-   ====================================================================
-   BIT  Symbol    Description                                   Default
-   ---  ------    --------------------------------------------- -------
-     7  BDU       Block Data Update (0=continuous, 1=LSB/MSB)         0
-     6  BLE       Big/Little-Endian (0=Data LSB, 1=Data MSB)          0
-   5-4  FS1/0     Full scale selection                               00
-                                  00 = 250 dps
-                                  01 = 500 dps
-                                  10 = 2000 dps
-                                  11 = 2000 dps
-     0  SIM       SPI Mode (0=4-wire, 1=3-wire)                       0 */
-
-  /* Adjust resolution if requested */
-  switch(range)
-  {
-    case L3DS20_RANGE_250DPS:
-      __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x00);
-      break;
-    case L3DS20_RANGE_500DPS:
-      __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x10);
-      break;
-    case L3DS20_RANGE_2000DPS:
-      __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x20);
-      break;
-  }
-  /* ------------------------------------------------------------------ */
-
-  /* Set CTRL_REG5 (0x24)
-   ====================================================================
-   BIT  Symbol    Description                                   Default
-   ---  ------    --------------------------------------------- -------
-     7  BOOT      Reboot memory content (0=normal, 1=reboot)          0
-     6  FIFO_EN   FIFO enable (0=FIFO disable, 1=enable)              0
-     4  HPen      High-pass filter enable (0=disable,1=enable)        0
-   3-2  INT1_SEL  INT1 Selection config                              00
-   1-0  OUT_SEL   Out selection config                               00 */
-
-  /* Nothing to do ... keep default values */
-  /* ------------------------------------------------------------------ */
-
-  return 1;
+uint8_t __L3GD20_read8(l3gd20Registers_t reg) {
+    uint8_t value;
+    GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
+    spi_send((uint8_t)reg | 0x80); // set READ bit
+    value = spi_send(0xFF);
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
+    return value;
 }
 
-void GyroReadXYZData(l3gd20Data * data)
-{ 
-  uint8_t xhi, xlo, ylo, yhi, zlo, zhi;
-
-  //digitalWrite(_cs, LOW);
-  GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
-
-  spi_send(L3GD20_REGISTER_OUT_X_L | 0x80 | 0x40); // SPI read, autoincrement
-  delay(10);
-  xlo = spi_send(0xFF);
-  xhi = spi_send(0xFF);
-  ylo = spi_send(0xFF);
-  yhi = spi_send(0xFF);
-  zlo = spi_send(0xFF);
-  zhi = spi_send(0xFF);
-
-  //digitalWrite(_cs, HIGH);
-  GYRO_CS_PxOUT |= GYRO_CS_BITx;
-
-  // Shift values to create properly formed integer (low uint8_t first)
-  data->x = (int16_t)(xlo | (xhi << 8));
-  data->y = (int16_t)(ylo | (yhi << 8));
-  data->z = (int16_t)(zlo | (zhi << 8));
-  
-  // Compensate values depending on the resolution
-  switch(range)
-  {
-    case L3DS20_RANGE_250DPS:
-      data->x *= L3GD20_SENSITIVITY_250DPS;
-      data->y *= L3GD20_SENSITIVITY_250DPS;
-      data->z *= L3GD20_SENSITIVITY_250DPS;
-      break;
-    case L3DS20_RANGE_500DPS:
-      data->x *= L3GD20_SENSITIVITY_500DPS;
-      data->y *= L3GD20_SENSITIVITY_500DPS;
-      data->z *= L3GD20_SENSITIVITY_500DPS;
-      break;
-    case L3DS20_RANGE_2000DPS:
-      data->x *= L3GD20_SENSITIVITY_2000DPS;
-      data->y *= L3GD20_SENSITIVITY_2000DPS;
-      data->z *= L3GD20_SENSITIVITY_2000DPS;
-      break;
-  }
+uint8_t __LSM6DSL_read8(lsm6dslRegisters_t reg) {
+    uint8_t value;
+    GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
+    spi_send((uint8_t)reg | 0x80); // set READ bit
+    value = spi_send(0xFF);
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
+    return value;
 }
 
-/* Set CTRL_REG1 (0x20)
- ====================================================================
- BIT  Symbol    Description                                   Default
- ---  ------    --------------------------------------------- -------
- 7-6  DR1/0     Output data rate                                   00
- 5-4  BW1/0     Bandwidth selection                                00
-   3  PD        0 = Power-down mode, 1 = normal/sleep mode          0
-   2  ZEN       Z-axis enable (0 = disabled, 1 = enabled)           1
-   1  YEN       Y-axis enable (0 = disabled, 1 = enabled)           1
-   0  XEN       X-axis enable (0 = disabled, 1 = enabled)           1 */
 
-/* Switch to normal mode and enable all three channels */
+int8_t GyroBegin() {
+    /* 1. Configure the Gyro's chip select pin (active low)  *
+     *    and set it's output to HIGH so it is not selected. */
+    GYRO_CS_PxDIR |= GYRO_CS_BITx;
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
+
+    /* 2. Set the chip address (if using I2C) and data range. */
+    #ifdef BSP_SNAIL_KITE
+        address = L3GD20_ADDRESS;
+    #endif
+    #ifdef BSP_SNAIL_KITE_D
+        address = LSM6DSL_ADDRESS;
+    #endif
+    range = L3DS20_RANGE_250DPS;
+
+    /* 3. Grab the gyro chip ID from the WHO_AM_I register. */
+    uint8_t id;
+    #ifdef BSP_SNAIL_KITE
+        id = __L3GD20_read8(L3GD20_REGISTER_WHO_AM_I);
+    #endif
+    #ifdef BSP_SNAIL_KITE_D
+        id = __LSM6DSL_read8(LSM6DSL_REGISTER_WHO_AM_I);
+    #endif
+
+    /* 4. If the returned ID does not match one of our supported    *
+    *    gyro chips, then exit without further chip configuration. */
+    if ((id != L3GD20_ID) && (id != L3GD20H_ID) && (id != LSM6DSL_ID)) {
+        return 0;
+    }
+
+    /* 5. Put the chip in PD mode. It should only be turned on *
+    *    if an application has a subscription to the gyro.    */
+    GyroPowerDownMode();
+
+    /* 6. Set the data range for the gyro. Both chips support *
+    *    250, 500, and 2000 so I didnt bother making another *
+    *    special enum type for the LSM6DSL chip.             */
+    switch(range) {
+        case L3DS20_RANGE_250DPS:
+            /* Kite.c - Bits 5-4 of CTRL_REG4 control dps, where *
+             *          00 = 250, 01 = 500, and 10 = 2000.       */
+            #ifdef BSP_SNAIL_KITE
+                __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x00);
+            #endif
+
+            /* Kite.d - Bits 3-2 of CTRL2_G control dps, where *
+             *          00 = 250, 01 = 500, and 11 = 2000.     */
+            #ifdef BSP_SNAIL_KITE_D
+                __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL2_G, 0x00);
+            #endif
+            break;
+
+        case L3DS20_RANGE_500DPS:
+            #ifdef BSP_SNAIL_KITE
+                __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x10);
+            #endif
+            #ifdef BSP_SNAIL_KITE_D
+                __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL2_G, 0x04);
+            #endif
+            break;
+
+        case L3DS20_RANGE_2000DPS:
+            #ifdef BSP_SNAIL_KITE
+                __L3GD20_write8(L3GD20_REGISTER_CTRL_REG4, 0x20);
+            #endif
+            #ifdef BSP_SNAIL_KITE_D
+                __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL2_G, 0x0C);
+            #endif
+            break;
+    }
+    return 1;
+}
+
+void GyroReadXYZData(l3gd20Data * data) {
+    /* 1. Set the gyros CS pin output to LOW to indicate *
+     *    that we wish to communicate with it via SPI.   */
+    uint8_t xhi, xlo, ylo, yhi, zlo, zhi;
+    GYRO_CS_PxOUT &= ~GYRO_CS_BITx;
+
+    /* 2. Kite.c - Send the address we wish to start reading from (gyro X *
+     *             output) and set the SPI read and autoincrement bits    *
+     *             (0x80, 0x40). Then read the low/high x,y,z registers.  *
+     *    Kite.d - Exact same as Kite.c, except we do not need the auto   *
+     *             increment bits as that is set in CTRL3_C in GyroBegin. */
+    #ifdef BSP_SNAIL_KITE
+        spi_send(L3GD20_REGISTER_OUT_X_L | 0x80 | 0x40);
+    #endif
+    #ifdef BSP_SNAIL_KITE_D
+        spi_send(LSM6DSL_REGISTER_OUTX_L_G | 0x80);
+    #endif
+    delay(10);
+    xlo = spi_send(0xFF);
+    xhi = spi_send(0xFF);
+    ylo = spi_send(0xFF);
+    yhi = spi_send(0xFF);
+    zlo = spi_send(0xFF);
+    zhi = spi_send(0xFF);
+
+    /* 4. Set the CS pin to HIGH to finish SPI communication. */
+    GYRO_CS_PxOUT |= GYRO_CS_BITx;
+
+    /* 5. By default, the gyro stores values in little endian order.      *
+     *    Combine byte-pairs with the lower one first to build x,y,z vals */
+    data->x = (int16_t)(xlo | (xhi << 8));
+    data->y = (int16_t)(ylo | (yhi << 8));
+    data->z = (int16_t)(zlo | (zhi << 8));
+
+    /* TODO: Is this the same for the newer gyro? */
+    /* 6. Compensate values depending on the resolution. */
+    switch(range) {
+        case L3DS20_RANGE_250DPS:
+            data->x *= L3GD20_SENSITIVITY_250DPS;
+            data->y *= L3GD20_SENSITIVITY_250DPS;
+            data->z *= L3GD20_SENSITIVITY_250DPS;
+            break;
+        case L3DS20_RANGE_500DPS:
+            data->x *= L3GD20_SENSITIVITY_500DPS;
+            data->y *= L3GD20_SENSITIVITY_500DPS;
+            data->z *= L3GD20_SENSITIVITY_500DPS;
+            break;
+        case L3DS20_RANGE_2000DPS:
+            data->x *= L3GD20_SENSITIVITY_2000DPS;
+            data->y *= L3GD20_SENSITIVITY_2000DPS;
+            data->z *= L3GD20_SENSITIVITY_2000DPS;
+        break;
+    }
+}
+
 void GyroPowerDownMode() {
-  __L3GD20_write8(L3GD20_REGISTER_CTRL_REG1, 0x07);
+    /* Kite.c - Set the output data rate (bits 7-4) to 95Hz, put the chip *
+     *          in power down mode (bit 3), and enable the Z, Y, and X    *
+     *          channels (bits 2-0).                                      */
+    #ifdef BSP_SNAIL_KITE
+        __L3GD20_write8(L3GD20_REGISTER_CTRL_REG1, 0x07);
+    #endif
+
+    /* Kite.d - The LSM6DSL has an accelerometer and gyro, and both need  *
+     *          to be off for full PD mode. Write 0s to bits 7-4 of       *
+     *          CTRL1_XL and CTRL2_G to turn off the accel and gyro.      *
+     *          Disable high-performance mode by setting bit 4 of CTRL6_C */
+    #ifdef BSP_SNAIL_KITE_D
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL1_XL, 0x00);
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL6_C, 0x10);
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL3_C, 0x04);
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL2_G, 0x00);
+    #endif
 }
 
 void GyroNormalMode() {
-  __L3GD20_write8(L3GD20_REGISTER_CTRL_REG1, 0x0F);
+    #ifdef BSP_SNAIL_KITE
+        __L3GD20_write8(L3GD20_REGISTER_CTRL_REG1, 0x0F);
+    #endif
+
+    /* Kite.d - Turn on the gyro (LEAVE ACCEL OFF!). Set to normal mode *
+     *          at 104Hz. Turn off high performance mode to save power. */
+    #ifdef BSP_SNAIL_KITE_D
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL2_G, 0x40);
+        __LSM6DSL_write8(LSM6DSL_REGISTER_CTRL6_C, 0x10);
+    #endif
 }
--- firmware/bsl/Gyro/gyro.h
+++ firmware/bsl/Gyro/gyro.h
@@ -22,9 +22,11 @@
 #include <stdint.h>
 
 #define L3GD20_ADDRESS                (0x6B)        // 1101011
+#define LSM6DSL_ADDRESS               (0x6B)        // 1101011
 #define L3GD20_POLL_TIMEOUT           (100)         // Maximum number of read attempts
 #define L3GD20_ID                     0xD4
 #define L3GD20H_ID                    0xD7
+#define LSM6DSL_ID                    0x6A
 
 #define L3GD20_SENSITIVITY_250DPS  (0.00875F)      // Roughly 22/256 for fixed point match
 #define L3GD20_SENSITIVITY_500DPS  (0.0175F)       // Roughly 45/256
@@ -60,6 +62,194 @@ typedef enum
   L3GD20_REGISTER_TSH_ZL              = 0x37,   // 00000000   rw
   L3GD20_REGISTER_INT1_DURATION       = 0x38    // 00000000   rw
 } l3gd20Registers_t;
+
+typedef enum
+{
+  // RESERVED                                         // DEFAULT    TYPE
+  LSM6DSL_REGISTER_RESERVED1                = 0x00,   //
+
+  // Embedded functions configuration register
+  LSM6DSL_REGISTER_FUNC_CFG_ACCESS          = 0x01,   // 00000000   rw
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED2                = 0x02,   //
+  LSM6DSL_REGISTER_RESERVED3                = 0x03,   //
+
+  // Sensor sync configuration registers
+  LSM6DSL_REGISTER_SENSOR_SYNC_TIME_FRAME   = 0x04,   // 00000000   rw
+  LSM6DSL_REGISTER_SENSOR_SYNC_RES_RATIO    = 0x05,   // 00000000   rw
+  
+  // FIFO configuration registers
+  LSM6DSL_REGISTER_FIFO_CTRL1               = 0x06,   // 00000000   rw
+  LSM6DSL_REGISTER_FIFO_CTRL2               = 0x07,   // 00000000   rw
+  LSM6DSL_REGISTER_FIFO_CTRL3               = 0x08,   // 00000000   rw
+  LSM6DSL_REGISTER_FIFO_CTRL4               = 0x09,   // 00000000   rw
+  LSM6DSL_REGISTER_FIFO_CTRL5               = 0x0A,   // 00000000   rw
+
+  // DataReady configuration register
+  LSM6DSL_REGISTER_DRDY_PULSE_CFG_G         = 0x0B,   // 00000000   rw
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED4                = 0x0C,   // 00000000   rw
+
+  // Interrupt pins 1 & 2 controls
+  LSM6DSL_REGISTER_INT1_CTRL                = 0x0D,   // 00000000   rw
+  LSM6DSL_REGISTER_INT2_CTRL                = 0x0E,   // 00000000   rw
+
+  // Who I am ID
+  LSM6DSL_REGISTER_WHO_AM_I                 = 0x0F,   // 01101010   r
+
+  // Accelerometer and Gyroscope control registers
+  LSM6DSL_REGISTER_CTRL1_XL                 = 0x10,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL2_G                  = 0x11,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL3_C                  = 0x12,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL4_C                  = 0x13,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL5_C                  = 0x14,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL6_C                  = 0x15,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL7_G                  = 0x16,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL8_XL                 = 0x17,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL9_XL                 = 0x18,   // 00000000   rw
+  LSM6DSL_REGISTER_CTRL10_C                 = 0x19,   // 00000000   rw
+
+  // I2C master configuration register
+  LSM6DSL_REGISTER_MASTER_CONFIG            = 0x1A,   // 00000000   rw
+
+  // Wake-up interrupt source register
+  LSM6DSL_REGISTER_WAKE_UP_SRC              = 0x1B,   // output     r
+
+  // Tap interrupt source register
+  LSM6DSL_REGISTER_TAP_SRC                  = 0x1C,   // output     r
+
+  // Portrait, landscape, face-up and face-down source register
+  LSM6DSL_REGISTER_D6D_SRC                  = 0x1D,   // output     r
+
+  // Status data register for user interface
+  LSM6DSL_REGISTER_STATUS_REG               = 0x1E,   // output     r
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED5                = 0x1F,   //
+
+  // Temperature output data registers
+  LSM6DSL_REGISTER_OUT_TEMP_L               = 0x20,   // output     r
+  LSM6DSL_REGISTER_OUT_TEMP_H               = 0x21,   // output     r
+
+  // Gyroscope output registers
+  LSM6DSL_REGISTER_OUTX_L_G                 = 0x22,   // output     r
+  LSM6DSL_REGISTER_OUTX_H_G                 = 0x23,   // output     r
+  LSM6DSL_REGISTER_OUTY_L_G                 = 0x24,   // output     r
+  LSM6DSL_REGISTER_OUTY_H_G                 = 0x25,   // output     r
+  LSM6DSL_REGISTER_OUTZ_L_G                 = 0x26,   // output     r
+  LSM6DSL_REGISTER_OUTZ_H_G                 = 0x27,   // output     r
+
+  // Acceleromter output registers
+  LSM6DSL_REGISTER_OUTX_L_XL                = 0x28,   // output     r
+  LSM6DSL_REGISTER_OUTX_H_XL                = 0x29,   // output     r
+  LSM6DSL_REGISTER_OUTY_L_XL                = 0x2A,   // output     r
+  LSM6DSL_REGISTER_OUTY_H_XL                = 0x2B,   // output     r
+  LSM6DSL_REGISTER_OUTZ_L_XL                = 0x2C,   // output     r
+  LSM6DSL_REGISTER_OUTZ_H_XL                = 0x2D,   // output     r
+
+  // Sensor hub output registers
+  LSM6DSL_REGISTER_SENSORHUB1_REG           = 0x2E,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB2_REG           = 0x2F,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB3_REG           = 0x30,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB4_REG           = 0x31,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB5_REG           = 0x32,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB6_REG           = 0x33,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB7_REG           = 0x34,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB8_REG           = 0x35,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB9_REG           = 0x36,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB10_REG          = 0x37,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB11_REG          = 0x38,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB12_REG          = 0x39,   // output     r
+
+  // FIFO status registers
+  LSM6DSL_REGISTER_FIFO_STATUS1             = 0x3A,   // output     r
+  LSM6DSL_REGISTER_FIFO_STATUS2             = 0x3B,   // output     r
+  LSM6DSL_REGISTER_FIFO_STATUS3             = 0x3C,   // output     r
+  LSM6DSL_REGISTER_FIFO_STATUS4             = 0x3D,   // output     r
+
+  // FIFO data output registers
+  LSM6DSL_REGISTER_FIFO_DATA_OUT_L          = 0x3E,   // output     r
+  LSM6DSL_REGISTER_FIFO_DATA_OUT_H          = 0x3F,   // output     r
+
+  // Timestamp output registers
+  LSM6DSL_REGISTER_TIMESTAMP0_REG           = 0x40,   // output     r
+  LSM6DSL_REGISTER_TIMESTAMP1_REG           = 0x41,   // output     r
+  LSM6DSL_REGISTER_TIMESTAMP2_REG           = 0x42,   // output     r
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED6                = 0x43,   //
+
+  // Step counter timestamp registers
+  LSM6DSL_REGISTER_STEP_TIMESTAMP_L         = 0x49,   // output     r
+  LSM6DSL_REGISTER_STEP_TIMESTAMP_H         = 0x4A,   // output     r
+
+  // Step counter output registers
+  LSM6DSL_REGISTER_STEP_COUNTER_L           = 0x4B,   // output     r
+  LSM6DSL_REGISTER_STEP_COUNTER_H           = 0x4C,   // output     r
+
+  // Sensor hub output registers
+  LSM6DSL_REGISTER_SENSORHUB13_REG          = 0x4D,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB14_REG          = 0x4E,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB15_REG          = 0x4F,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB16_REG          = 0x50,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB17_REG          = 0x51,   // output     r
+  LSM6DSL_REGISTER_SENSORHUB18_REG          = 0x52,   // output     r
+
+  // Significant motion, tilt, step detector, hard/soft-iron
+  // and sensor hub interrupt source register
+  LSM6DSL_REGISTER_FUNC_SRC1                = 0x53,   // output     r
+
+  // Wrist tilt registers
+  LSM6DSL_REGISTER_FUNC_SRC2                = 0x54,   // output     r
+  LSM6DSL_REGISTER_WRIST_TILT_IA            = 0x55,   // output     r
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED7                = 0x56,   //
+
+  // Tap configuration and interrupt registers
+  LSM6DSL_REGISTER_TAP_CFG                  = 0x58,   // 00000000   rw
+  LSM6DSL_REGISTER_TAP_THIS_6D              = 0x59,   // 00000000   rw
+  LSM6DSL_REGISTER_INT_DUR2                 = 0x5A,   // 00000000   rw
+  LSM6DSL_REGISTER_WAKE_UP_THS              = 0x5B,   // 00000000   rw
+  LSM6DSL_REGISTER_WAKE_UP_DUR              = 0x5C,   // 00000000   rw
+
+  // Free-fall function duration setting register
+  LSM6DSL_REGISTER_FREE_FALL                = 0x5D,   // 00000000   rw
+
+  // Functions routing on int 1 and 2 pins
+  LSM6DSL_REGISTER_MD1_CFG                  = 0x5E,   // 00000000   rw
+  LSM6DSL_REGISTER_MD2_CFG                  = 0x5F,   // 00000000   rw
+
+  // Master command code used for stamping for sensor sync
+  LSM6DSL_REGISTER_MASTER_CMD_CODE          = 0x60,   // 00000000   rw
+
+  // Error code used for sensor synchronization
+  LSM6DSL_REGISTER_SENS_SYNC_SPI_ERROR_CODE = 0x61,   // 00000000   rw
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED8                = 0x62,   //
+
+  // External magnetometer raw data output registers
+  LSM6DSL_REGISTER_OUT_MAG_RAW_X_L          = 0x66,   // output     r
+  LSM6DSL_REGISTER_OUT_MAG_RAW_X_H          = 0x67,   // output     r
+  LSM6DSL_REGISTER_OUT_MAG_RAW_Y_L          = 0x68,   // output     r
+  LSM6DSL_REGISTER_OUT_MAG_RAW_Y_H          = 0x69,   // output     r
+  LSM6DSL_REGISTER_OUT_MAG_RAW_Z_L          = 0x6A,   // output     r
+  LSM6DSL_REGISTER_OUT_MAG_RAW_Z_H          = 0x6B,   // output     r
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED9                = 0x6C,   //
+
+  // Accelerometer user offset correction
+  LSM6DSL_REGISTER_X_OFS_USR                = 0x73,   // 00000000   rw
+  LSM6DSL_REGISTER_Y_OFS_USR                = 0x74,   // 00000000   rw
+  LSM6DSL_REGISTER_Z_OFS_USR                = 0x75,   // 00000000   rw
+
+  // RESERVED
+  LSM6DSL_REGISTER_RESERVED10               = 0x76    //
+} lsm6dslRegisters_t;
 
 typedef enum
 {
